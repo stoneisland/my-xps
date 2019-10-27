2f16aea3df55010f7c38c0415cb66f22aa635ef0
stoneisland <gianluca.fioletti@gmail.com>
2019-10-27 15:21:55 +0100

    Add comment

--- modules.sh
+++ modules.sh
@@ -5,10 +5,11 @@ cd /tmp
 openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -days 36500 -subj "/CN=luca/"
 
 sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n iwlmvm)
+
+#Dependencies (modinfo iwlmvm)
 sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n mac80211)
 sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n iwlwifi)
 sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n cfg80211)
 sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n compat)
 
 sudo mokutil --import MOK.der
-
